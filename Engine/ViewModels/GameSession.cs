1d7043e4d3a34a16505f69f5aea912091ede9b5d
ZeroAT <zeroat@live.com>
2019-06-20 14:52:35 -0400

    Player Model

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,23 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.ViewModels
+{
+    class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Abdul";
+            CurrentPlayer.Gold = 1000000;
+
+        }
+
+    }
+}
